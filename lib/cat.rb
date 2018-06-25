2e1871c64cbba4da3e69a3d1dd05e586eb0002f2
neah101 <neah.shaw2005@gmail.com>
2018-06-25 12:53:50 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
-  # code goes here
+  attr_accessors:name,:color,:gender,:age
+puts "on a scale of 1-10 how is your cat feeling"
+feelings=gets.chomp
+if feelings>5
+  puts " i am happy cat because i have food"
+elsif feelings=5
+puts "i dont feel to bad"
+else feelings<5
+  puts " i am a sad cat feed me now"
+
 end
+  

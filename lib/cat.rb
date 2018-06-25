b2b19fe20348e2ee3817bb63eb84b52a3d7167c4
neah101 <neah.shaw2005@gmail.com>
2018-06-25 13:16:20 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,5 +1,6 @@
 class Cat
   attr_accessors:name,:color,:gender,:age
+  feelings=0
 puts "on a scale of 1-10 how is your cat feeling"
 feelings=gets.chomp
 if feelings>5
@@ -8,6 +9,12 @@ elsif feelings=5
 puts "i dont feel to bad"
 else feelings<5
   puts " i am a sad cat feed me now"
-
 end
+end 
+ puts "what is your cat doing at this piont in time"
+ if doing="walking"
+   puts "you increased your mood by 2 points"
+   
+   
+ 
   
